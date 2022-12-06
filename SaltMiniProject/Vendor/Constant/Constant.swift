c8863390075fd9259389f5b90f7b412efc156d82
Gratianus Martin Brianto <martin.gratianus@hotmail.com>
2022-12-06 16:57:51 +0700

    tidy up constant

--- SaltMiniProject/Vendor/Constant/Constant.swift
+++ SaltMiniProject/Vendor/Constant/Constant.swift
@@ -7,18 +7,6 @@
 
 import Foundation
 
-struct Constant {
+internal struct Constant {
     static let Url = "https://reqres.in/api/login"
-    
-    enum ContentType: String {
-            case json = "application/json"
-        }
-    
-    enum ApiError: Int, Error {
-        case forbidden = 403              //Status code 403
-        case notFound = 404               //Status code 404
-        case conflict = 409               //Status code 409
-        case internalServerError = 500    //Status code 500
-        case badRequest = 400              //Status code 400
-    }
 }
--- SaltMiniProject/Vendor/Manager/NetworkManager.swift
+++ SaltMiniProject/Vendor/Manager/NetworkManager.swift
@@ -9,6 +9,14 @@ import Alamofire
 import RxSwift
 import Foundation
 
+internal enum ApiError: Int, Error {
+    case forbidden = 403              //Status code 403
+    case notFound = 404               //Status code 404
+    case conflict = 409               //Status code 409
+    case internalServerError = 500    //Status code 500
+    case badRequest = 400              //Status code 400
+}
+
 internal class NetworkManager {
     internal func postLogin(body: LoginPost, completion: @escaping (Result<LoginResponse,Error>)->()) {
         
@@ -24,7 +32,7 @@ internal class NetworkManager {
                         completion(.success(value))
                     }
                 case .failure(let error):
-                    if let statusCode = response.response?.statusCode, let apiError = Constant.ApiError(rawValue: statusCode) {
+                    if let statusCode = response.response?.statusCode, let apiError = ApiError(rawValue: statusCode) {
                         completion(.failure(apiError))
                     }
                     completion(.failure(error))
